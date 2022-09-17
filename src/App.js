3bd21d26dd9e91d0a6c239a637bcf5c832d9996a
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-09-17 15:33:41 +0300

    solveerr

--- src/App.js
+++ src/App.js
@@ -1,35 +1,66 @@
-import axios from 'axios';
-import { useEffect,useState } from 'react';
-import './App.css';
-import AddComment from './components/AddComment';
-import AddPost from './components/AddPost';
-import ShowData from './components/ShowData';
+import Main from './components/Main'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData().then((resolve) => {
+      setData(resolve);
+    });
 
-  const [data ,setData] = useState()
+  }, []);
 
- const getData =async ()=>{
+  async function getData() {
+    let url = `${process.env.REACT_APP_SERVER}/post`;
+    const axiosRequest = await axios.get(url);
+    const datax = axiosRequest.data;
+    return datax;
 
-  let tempData = await axios.get("https://whitebord-backend.herokuapp.com/post/")
+  }
 
-  setData(tempData.data)
+  async function deletePost(id) {
+    let url = `${process.env.REACT_APP_SERVER}/post/${id}`;
+    await axios.delete(url);
+    getData().then((resolve) => {
+      setData(resolve);
+    });
 
- }
+  }
 
+  async function addComment(id, obj) {
+    let url = `${process.env.REACT_APP_SERVER}/comment/${id}`;
+    if (obj.text === '') {
+      alert(`add comment before submit`)
+    } else {
+      let comments = await axios.post(url, obj);
+      getData().then((resolve) => {
+        setData(resolve);
+      })
+    }
 
-  useEffect(()=>{
-      getData()
-  })
+  }
+
+  async function addPost(e) {
+    e.preventDefault();
+    const obj = {
+      title:e.target.title.value,
+      content:e.target.contnet.value,
+    }
+    let url = `${process.env.REACT_APP_SERVER}/post`;
+    let axiosRequest = await axios.post(url, obj);
+    let posts = axiosRequest.data;
+    getData().then((resolve) => {
+      setData(resolve);
+    })
+
+  }
 
   return (
-    <div className="App">
-
-      <AddPost getDataFun={getData}/>
-      <AddComment getDataFun={getData}/>
-      <ShowData data={data}/>
-
-    </div>
+    <>
+      <Main data={data} dfunc={deletePost} acfunc={addComment} apfunc={addPost}/>
+    </>
   );
 }
 
